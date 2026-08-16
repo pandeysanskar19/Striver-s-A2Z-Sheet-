59c3b02e77bbd5a8b94c3cf86a11f8ff69c9f29d
Sanskar Pandey <pandeysanskar1809@gmail.com>
2026-08-16 23:56:52 +0530

    Create Second Largest..cpp
    Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
    
    Note: The second largest element should not be equal to the largest element.
    
    Examples:
    
    Input: arr[] = [12, 35, 1, 10, 34, 1]
    Output: 34
    Explanation: The largest element of the array is 35 and the second largest element is 34.
    Input: arr[] = [10, 5, 10]
    Output: 5
    Explanation: The largest element of the array is 10 and the second largest element is 5.
    Input: arr[] = [10, 10, 10]
    Output: -1
    Explanation: The largest element of the array is 10 and the second largest element does not exist.
    Constraints:
    2 ≤ arr.size() ≤ 10^5
    1 ≤ arr[i] ≤ 10^5

---
+++ Arrays/Easy/Second Largest..cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        int n=arr.size();
+        
+        // code here
+//APPROACH 1:TIME COMPLEXITY O(N) , SPACE COMPLEXITY: O(1).
+        int largest=arr[0];
+        int slargest=-1;
+        for(int i=1;i<n;i++){
+            if(arr[i]>largest){
+                slargest=largest;
+                largest=arr[i];
+                
+            }
+            else if(arr[i]>slargest && arr[i]<largest){
+                slargest=arr[i];
+            }
+        }
+        return slargest;
+    }
+};
